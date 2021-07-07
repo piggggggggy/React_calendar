e39a7f06ac6618cd09ac167f6950ec7a7ee05a68
YONG-TAE PARK <pyt4105@gmail.com>
2021-07-08 06:06:32 +0900

    :poop: calendar모듈 셋팅

--- src/redux/modulse/calendar.js
+++ src/redux/modulse/calendar.js
@@ -5,3 +5,27 @@ import moment from "moment";
 
 
 // actions
+const DATE_NOW = "date/DATE_NOW";
+// const DATE_CHG = "date/DATE_CHG";
+
+
+// action creator
+const setDate = createAction(DATE_NOW, (date) => ({date}));
+
+
+
+// initialState
+const initialState = {
+    now : moment(),
+};
+
+
+
+//reducer
+
+
+export default handleActions({
+    [DATE_NOW]: (state, action) => produce(state, (draft) => {
+        
+    }),
+})
